64deebd80ea6c736cd6695f187bd1b4f8b21d793
Pinolpier <14354249+Pinolpier@users.noreply.github.com>
2020-06-17 16:14:31 +0200

    fixed small copy bug

--- src/main/java/campuscup/localangels/backend/repository/MerchantRepository.java
+++ src/main/java/campuscup/localangels/backend/repository/MerchantRepository.java
@@ -1,7 +1,6 @@
 package campuscup.localangels.backend.repository;
 
 import campuscup.localangels.backend.model.Merchant;
-import campuscup.localangels.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,5 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MerchantRepository extends JpaRepository<Merchant, Long> {
     @Query("SELECT m FROM merchant m WHERE m.email = ?1")
-    User findMerchantByEmail(String email);
+    Merchant findMerchantByEmail(String email);
 }
